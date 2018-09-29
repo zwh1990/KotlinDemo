513fa690fdb5e2aa48819afcd3cd7449bbaac5dd
zhaowh <1247615020@qq.com>
2018-09-29 11:28:09 +0800

    添加 if for示例

--- app/src/main/java/test/com/jzd/kotlintestapplication/ForActivity.kt
+++ app/src/main/java/test/com/jzd/kotlintestapplication/ForActivity.kt
@@ -18,22 +18,63 @@ class ForActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_for)
 
-        forItor()
+//        forItor()
+//        whiles()
+        interrupt()
+
     }
 
-    fun forItor(){
 
-        val items = listOf("apple","orange","kiwi")
+    //for 遍历
+    fun forItor() {
+
+        val items = listOf("apple", "orange", "kiwi")
 //        for (item in items){
 //            Log.w(TAG,"item--->" + item)
 //        }
 
-
-        for (index in items.indices){
-            Log.w(TAG,"index--->" + items[index])
+        for (index in items.indices) {
+            Log.w(TAG, "index--->" + items[index])
         }
+
     }
 
+
+    //while 的使用   x和y的值都是5 输出结果相同，都为0 则只有y输出值
+    fun whiles() {
+
+        var x = 0
+        while (x > 0) {
+            Log.w(TAG, "x--->" + x)
+            x --
+        }
+
+        var y = 0
+        do {
+            Log.w(TAG, "y--->" + y)
+            y --
+        } while (y > 0)
+
+    }
+
+    //终止字符的使用
+    fun interrupt() {
+        for (i in 1..10) {
+            for (j in 2..5) {
+                if (j == 4) {
+                    //return 是终止两层循环
+//                    return
+                    //break 是终止内部循环
+//                    break
+
+                    //continue 是跳过内部j == 4循环
+                    continue
+                }
+                Log.w(TAG, "j--->" + j)
+            }
+            Log.w(TAG, "i--->" + i)
+        }
+    }
 
 
 }
